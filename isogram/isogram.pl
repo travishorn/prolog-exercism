7cebbf68bacaac73f33f9f1cbd1f12eec36d67c4
Travis Horn <travis@travishorn.com>
2023-05-02 17:36:22 -0500

    Complete Isogram

--- isogram/isogram.pl
+++ isogram/isogram.pl
@@ -1,1 +1,24 @@
-isogram(false).
+% Load the module for Perl compatible regular expression matching
+:- use_module(library(pcre)).
+
+% Determine if a word or phrase is an isogram
+isogram(Word) :-
+
+  % Convert the word to all uppercase letters
+  string_upper(Word, UppercaseWord),
+
+  % Remove any character that is not an uppercase letter
+  re_replace("[^A-Z]"/g, "", UppercaseWord, CleanWord),
+
+  % Split the word into a list of uppercase letters
+  atom_chars(CleanWord, Letters),
+
+  % Sort the letters. **This predicate also removes duplicates.**
+  sort(Letters, SortedLetters),
+
+  % Bind the number of letters in the de-duplicated list to NumLetters
+  length(SortedLetters, NumLetters),
+
+  % Check the number of letters in the original list against NumLetters. If it
+  % is equivalent, isogram/1 is true.
+  length(Letters, NumLetters).
